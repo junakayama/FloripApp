4fb8844be8be4b36db7c0818a44afc673e1a3231
Julia <junakayama97@gmail.com>
2016-11-16 19:48:16 -0200

    MapsActivity adicionado e funcionando

--- app/src/main/java/com/example/leon/floripapp/MainActivity.java
+++ app/src/main/java/com/example/leon/floripapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.leon.floripapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,11 +16,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
-        Intent intent = new Intent(this,DisplayMessageActivity.class);
-        EditText editText =(EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
+//        Intent intent = new Intent(this,DisplayMessageActivity.class);
+//        EditText editText =(EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE,message);
+//
+//        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+        Intent mapIntent = new Intent(this,MapsActivity.class);
+//        mapIntent.setPackage("com.google.android.apps.maps");
+
+        startActivity(mapIntent);
+
     }
+
+//    public void startMapsActivity() {
+//        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//        mapIntent.setPackage("com.google.android.apps.maps");
+//        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(mapIntent);
+//        }
+//    }
+//
+//
+//    public void onClick(View view) {
+//        startMapsActivity();
+//    }
+
 
 }
