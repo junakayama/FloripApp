f47522d6ebe68de335ceb47769c7d8bbc0ad1cc3
leondaros <leondaros@users.noreply.github.com>
2016-11-18 21:49:29 -0200

    Criação ArrayAdapter

--- app/src/main/java/com/example/leon/floripapp/MainActivity.java
+++ app/src/main/java/com/example/leon/floripapp/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.leon.floripapp;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] pontos ={"Lugar1","Lugar2","Lugar3"};
+        ListView lista = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,pontos);
+        lista.setAdapter(adapter);
     }
 
-    public void sendMessage(View view){
+  /*  public void sendMessage(View view){
 //        Intent intent = new Intent(this,DisplayMessageActivity.class);
 //        EditText editText =(EditText) findViewById(R.id.edit_message);
 //        String message = editText.getText().toString();
@@ -30,21 +33,5 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(mapIntent);
 
-    }
-
-//    public void startMapsActivity() {
-//        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
-//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-//        mapIntent.setPackage("com.google.android.apps.maps");
-//        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(mapIntent);
-//        }
-//    }
-//
-//
-//    public void onClick(View view) {
-//        startMapsActivity();
-//    }
-
-
+    }*/
 }
