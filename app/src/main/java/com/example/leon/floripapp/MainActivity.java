fd2761150f6c377c8062d1c5bb144a46bda7178f
Julia <junakayama97@gmail.com>
2016-11-19 01:53:36 -0200

    Activity das informações criado sem os atributos

--- app/src/main/java/com/example/leon/floripapp/MainActivity.java
+++ app/src/main/java/com/example/leon/floripapp/MainActivity.java
@@ -15,33 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view){
-//        Intent intent = new Intent(this,DisplayMessageActivity.class);
-//        EditText editText =(EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE,message);
-//
-//        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+    public void startMapsActivity(View view){
         Intent mapIntent = new Intent(this,MapsActivity.class);
-//        mapIntent.setPackage("com.google.android.apps.maps");
 
         startActivity(mapIntent);
 
     }
-
-//    public void startMapsActivity() {
-//        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
-//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-//        mapIntent.setPackage("com.google.android.apps.maps");
-//        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(mapIntent);
-//        }
-//    }
-//
-//
-//    public void onClick(View view) {
-//        startMapsActivity();
-//    }
-
-
 }
